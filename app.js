3cf8125f5390df2d7d10fa9f863c7cebf5e6ca20
Nick Arcuri <nickarcuri@protonmail.com>
2018-09-01 13:35:58 -0400

    Test route

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.post('/test', (req, res) => {
+  res.send('ArtistHunt');
+});
 require('./routes/user')(app);
 
 app.listen(PORT || 8083, () => console.log(`Server listening on port ${PORT || 8083}`));
